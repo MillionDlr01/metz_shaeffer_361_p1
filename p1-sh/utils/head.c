d5bb313cbe214461d40b1cb7b5624dbb787739df
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2023-09-27 17:56:18 -0400

    head.c completed

--- p1-sh/utils/head.c
+++ p1-sh/utils/head.c
@@ -1,14 +1,89 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 // You may assume that lines are no longer than 1024 bytes
 #define LINELEN 1024
+// No command line can be more than 100 characters
+#define MAXLENGTH 100
+/* newline, and carriage return to match whitespace between tokens */
+#define WHITESPACE "\n\r"
 
 static void usage (void);
 
 int
 main (int argc, char *argv[])
 {
+  // cmd char | number of lines to print (5 default)
+  int ch = 0, numLines = 5;
+
+  // parse optional flag
+  while ((ch = getopt (argc, argv, "n:")) != -1)
+    {
+      switch (ch)
+        {
+        case 'n': // only option (sets numLines and nothing else happens)
+          numLines = atoi (optarg);
+          break;
+        default:
+          printf ("bad argument flag");
+          usage ();
+          return EXIT_FAILURE;
+        }
+    }
+
+  // parse optional filename
+  if (optind == argc) // this means no file nam was passed; read from stdin
+    {
+      // get buffer
+      char buffer[LINELEN + 1];
+
+      // print number of lines
+      for (int i = 0; i < numLines; i++)
+        {
+          memset (buffer, 0, LINELEN + 1);
+          fgets (buffer, MAXLENGTH, stdin);
+          printf ("%s", buffer);
+        }
+    }
+  else if (optind == argc - 1) // this means a file name has been passed
+    {
+      char *filename = argv[optind];
+      // attempt to open file
+      FILE *file = fopen (filename, "r");
+      // check that the file was opened
+      if (file == NULL)
+        {
+          printf ("file does not exist");
+          usage ();
+          return EXIT_FAILURE;
+        }
+
+      // print number of lines
+      for (int i = 0; i < numLines; i++)
+        {
+          // make line buffer
+          char buffer[LINELEN + 1];
+          memset (buffer, 0, LINELEN + 1);
+          // read line
+          fread (buffer, sizeof (char), LINELEN, file);
+          // print line
+          char *line = strtok (buffer, WHITESPACE);
+          while (line != NULL && i < numLines) {
+            printf ("%s\n", line);
+            line = strtok(NULL, WHITESPACE);
+            i++;
+          }
+        }
+    }
+  else
+    { // this means too many args have been passed.
+      printf ("too many non flag args passed");
+      usage ();
+      return EXIT_FAILURE;
+    }
   return EXIT_SUCCESS;
 }
 
