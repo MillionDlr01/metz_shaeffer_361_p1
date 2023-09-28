f3ccc9048231898cdf2c49072ffa3658c513c3dd
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2023-09-28 18:02:12 -0400

    started on env

--- p1-sh/utils/env.c
+++ p1-sh/utils/env.c
@@ -1,11 +1,29 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static void usage (void);
 
 int
 main (int argc, char *argv[], char *envp[])
 {
+  if (optind == argc) // no args passed, loop through envp and print
+    {
+
+      return EXIT_SUCCESS; // exit at the end of noarg operaion
+    }
+  // if no args passed
+  // create new envp array
+
+  // populate envp with the args
+
+  // get program name
+
+  // construct args
+
+  // spawn process
+
   return EXIT_SUCCESS;
 }
 
