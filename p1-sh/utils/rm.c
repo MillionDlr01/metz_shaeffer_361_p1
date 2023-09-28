599743f45a14c75b528435402431714ca4e99506
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2023-09-28 17:44:09 -0400

    rm complete; no tests present so ig im done

--- p1-sh/utils/rm.c
+++ p1-sh/utils/rm.c
@@ -1,11 +1,27 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static void usage (void);
 
 int
 main (int argc, char *argv[])
 {
+  // parse filename
+  if (optind == argc - 1) // this means a file name was passed
+    {
+      char *filename = argv[optind];
+      if (remove (filename) != 0)
+        {
+          return EXIT_FAILURE;
+        }
+    }
+  else
+    {
+      return EXIT_FAILURE;
+    }
+
   return EXIT_SUCCESS;
 }
 
