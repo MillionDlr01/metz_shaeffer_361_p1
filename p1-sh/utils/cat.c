4da9e9d41e3dbc088bd7f2acdc2936293da79e09
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2023-09-28 20:38:43 -0400

    cat done

--- p1-sh/utils/cat.c
+++ p1-sh/utils/cat.c
@@ -1,11 +1,31 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 static void usage (void);
 
 int
 main (int argc, char *argv[])
 {
+  // parse filename
+  char *filename = argv[optind];
+
+  // open file
+  FILE *file = fopen (filename, "r");
+  if (file == NULL)
+    {
+      return EXIT_FAILURE;
+    }
+
+  // print contents
+  char buffer [2048];
+  memset(buffer, 0, 2048);
+  while (fread(buffer, sizeof(char), 2047, file) != 0)  {
+    printf("%s", buffer);
+    memset(buffer, 0, 2048);
+  }
   return EXIT_SUCCESS;
 }
 
